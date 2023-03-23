3e818c8975830246cdf9e14272fa76037cea4d87
Sxnty <sxntycsgo@gmail.com>
2023-03-22 23:13:57 -0300

    added home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+
+import "../styles/home.scss";
+import FeaturedProducts from "./FeaturedProducts";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <main className="hero">
+        <div className="hero__info">
+          <h2>Lorem ipsum dolor sit.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+            autem deleniti quae fugit incidunt.
+          </p>
+          <p className="info__catalogue">Catalogue</p>
+        </div>
+      </main>
+      <FeaturedProducts />
+    </>
+  );
 }
 
-export default Home
+export default Home;
